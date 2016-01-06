fe4043597c1ee4b11a55eaa68f83bc4506253b6b
varontron <david.varon@novartis.com>
2016-01-05 19:17:28 -0500

    mod license copy and org name

--- yada-api/src/main/java/com/novartis/opensource/yada/format/FileSystemResultJSONConverter.java
+++ yada-api/src/main/java/com/novartis/opensource/yada/format/FileSystemResultJSONConverter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Novartis Institutes for BioMedical Research Inc.
+ * Copyright 2016 Novartis Institutes for BioMedical Research Inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * 
  */
