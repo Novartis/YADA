e7522b6241a9c0cbf3d1e44e9d9f46f1fcf87e25
varontron <david.varon@novartis.com>
2016-08-02 01:20:58 -0400

    corrected 'save as...' issue

--- yada-war/src/main/resources/scripts/YADA_db_uni_revisions_7_1_0.sql
+++ yada-war/src/main/resources/scripts/YADA_db_uni_revisions_7_1_0.sql
@@ -1,4 +1,13 @@
-UPDATE YADA_QUERY SET QUERY = 'select a.*,count(b.target) DEFAULT_PARAMS 
+CREATE TABLE IF NOT EXISTS YADA_PROP
+( 
+  TARGET VARCHAR(500), 
+  NAME VARCHAR(500),
+  VALUE VARCHAR(4000)
+);
+ALTER TABLE YADA_PARAMS RENAME TO YADA_PARAM;
+ALTER TABLE YADA_PARAM ADD ID VARCHAR(255);
+
+UPDATE YADA_QUERY SET QUERY = 'select a.*,count(b.target) default_params 
 from yada_query a left 
 join yada_param b on a.qname = b.target 
 where app = ?v 
@@ -13,4 +22,31 @@ a.modified_by,
 a.created,
 a.created_by,
 a.comments' WHERE QNAME = 'YADA queries';
-UPDATE YADA_QUERY SET QUERY = 'delete from YADA_PARAM where target = ?v and name = ?v and value = ?v and rule = ?i' WHERE QNAME = 'YADA delete default param';
+UPDATE YADA_QUERY SET QUERY = 'update yada_param set value = ?v, rule = ?i where target = ?v and name = ?v' WHERE QNAME = 'YADA update default param';
+UPDATE YADA_QUERY SET QUERY = 'SELECT * FROM YADA_PARAM
+where target in (?v) 
+  and target 
+  not in (''YADA apps'',
+          ''YADA queries'',
+          ''YADA new query'',
+          ''YADA delete query'',
+          ''YADA insert usage log'',
+          ''YADA update query'')' WHERE QNAME = 'YADA select default params';
+UPDATE YADA_QUERY SET QUERY = 'SELECT
+id as "ID", 
+target as "TARGET",
+name as "NAME",
+value as "VALUE",
+rule as "RULE"
+FROM YADA_PARAM where target in (select qname from yada_query where app = ?v and qname not in (''YADA apps'',''YADA queries'',''YADA new query'',''YADA delete query'',''YADA insert usage log'',''YADA update query''))'
+WHERE QNAME = 'YADA select default params for app';
+UPDATE YADA_QUERY SET QUERY = 'insert into YADA_PARAM (id, target, name, value, rule) values (?v,?v,?v,?v,?i)' WHERE QNAME = 'YADA insert default param';
+UPDATE YADA_QUERY SET QUERY = 'delete from YADA_PARAMS where target = ?v and name = ?v and value = ?v and rule = ?i' WHERE QNAME = 'YADA delete default param';
+
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA insert prop','insert into yada_prop (target, name, value) values (?v,?v,?v)','YADABOT','YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA update prop','update yada_prop set value = ?v where target = ?v and name = ?v', 'YADABOT', 'YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA delete prop','delete from yada_prop where target = ?v and name = ?v and value = ?v', 'YADABOT', 'YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA select props for target','select target as "TARGET", name as "NAME", value as "VALUE" from yada_prop where target = ?v', 'YADABOT', 'YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA select props for targets','select target as "TARGET", name as "NAME", value as "VALUE" from yada_prop where target in (?v)', 'YADABOT', 'YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA select props like target','select target as "TARGET", name as "NAME", value as "VALUE" from yada_prop where target like ?v||''%''', 'YADABOT', 'YADA');
+INSERT into YADA_QUERY (qname,query,created_by,app) VALUES ('YADA update protector for target','update yada_a11n set qname = ?v, type = ?v where target = ?v', 'YADABOT', 'YADA');
