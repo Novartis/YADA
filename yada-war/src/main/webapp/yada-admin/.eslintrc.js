1914aa4687929f5633419fa9f2a5048575dab452
varontron <varontron@gmail.com>
2019-03-28 11:07:54 -0400

    lint rules

--- yada-war/src/main/webapp/yada-admin/.eslintrc.js
+++ yada-war/src/main/webapp/yada-admin/.eslintrc.js
@@ -1,6 +1,7 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
+
   root: true,
   parserOptions: {
     parser: 'babel-eslint'
@@ -11,7 +12,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -25,5 +26,10 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-mixed-spaces-and-tabs": [0],
+    "no-tabs": 0,
+    "skipBlankLines": 0,
+    "ignoreComments": 0,
+    "no-trailing-spaces": [2, { "skipBlankLines": true }]
   }
 }
