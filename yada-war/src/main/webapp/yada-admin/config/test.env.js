356a8465e80328234d22f58c8ec006890bb7c54e
varontron <varontron@gmail.com>
2019-08-08 23:48:08 -0400

    test config

--- yada-war/src/main/webapp/yada-admin/config/test.env.js
+++ yada-war/src/main/webapp/yada-admin/config/test.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-NODE_ENV: '"testing"',
+NODE_ENV: '"development"',
   // label is used in header, to inform developer/tester, or suppress itself in PROD
   NODE_ENV_LABEL: '"TEST"',
   YADA: {
