6f0544a5332750e801c854fe42b9ec859f1e0589
varontron <varontron@gmail.com>
2020-03-02 00:29:19 -0500

    Enabled annotation inheritance

--- yada-api/src/main/java/com/novartis/opensource/yada/plugin/SecurityPreprocessor.java
+++ yada-api/src/main/java/com/novartis/opensource/yada/plugin/SecurityPreprocessor.java
@@ -7,16 +7,19 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * @author dvaron
+ * @since 8.7.6
+ */
+@Inherited
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
-/**
- * @author dvaron
- *
- */
+
 @interface SecurityPreprocessor {
 
 }
