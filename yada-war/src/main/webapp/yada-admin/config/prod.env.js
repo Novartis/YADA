4cc38ee940dd914aff31de70d2d943518a1e2878
David Varon <varontron@gmail.com>
2020-07-08 01:01:24 -0400

    removed url vals

--- yada-war/src/main/webapp/yada-admin/config/prod.env.js
+++ yada-war/src/main/webapp/yada-admin/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  YADA_BASEURL : '"http://yada.fog-inter.com"',
-  YADA_ADMIN_BASEURL : '"http://yada.fog-inter.com"',
+  YADA_BASEURL : '""',
+  YADA_ADMIN_BASEURL : '""',
 }
