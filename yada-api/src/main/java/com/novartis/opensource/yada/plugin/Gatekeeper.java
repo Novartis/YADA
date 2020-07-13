a8cebdb8b5e255892e1d613f11848182a0bd3b66
David Varon <varontron@gmail.com>
2020-07-13 12:33:04 -0400

    Added addl cast to string

--- yada-api/src/main/java/com/novartis/opensource/yada/plugin/Gatekeeper.java
+++ yada-api/src/main/java/com/novartis/opensource/yada/plugin/Gatekeeper.java
@@ -818,7 +818,7 @@ public class Gatekeeper extends AbstractPreprocessor {
 		String user = "";
 		try 
 		{
-			user = new JSONObject(obtainIdentity()).getString(Authorization.YADA_IDENTITY_SUB);
+			user = new JSONObject((String)obtainIdentity()).getString(Authorization.YADA_IDENTITY_SUB);
 		} 
 		catch (YADASecurityException | JSONException | YADARequestException | YADAExecutionException e)
 		{
