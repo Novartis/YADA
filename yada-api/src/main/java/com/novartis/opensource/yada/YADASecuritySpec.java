a9309d6107990550821759888fbc59f7b06772e5
David Varon <varontron@gmail.com>
2020-09-06 09:32:04 -0400

    removed policy key checks, fixed regex bug

--- yada-api/src/main/java/com/novartis/opensource/yada/YADASecuritySpec.java
+++ yada-api/src/main/java/com/novartis/opensource/yada/YADASecuritySpec.java
@@ -5,12 +5,14 @@ package com.novartis.opensource.yada;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -95,7 +97,7 @@ public class YADASecuritySpec extends HashMap<String, Object> {
    * 
    * @since 9.0.0 (moved from {@link com.novartis.opensource.yada.plugin.Gatekeeper}
    */
-  public static final String RX_COL_INJECTION = "(([a-zA-Z0-9_]+):)?(get[A-Z][a-zA-Z0-9_]+\\([A-Za-z0-9_]*\\))";
+  public static final String RX_COL_INJECTION = "(?:([a-zA-Z0-9_]+):)?(get[A-Z][a-zA-Z0-9_]+\\(([A-Za-z0-9_]*)\\))";
 
   
   /**
@@ -103,20 +105,20 @@ public class YADASecuritySpec extends HashMap<String, Object> {
    * 
    * @since 9.0.0 (moved from {@link com.novartis.opensource.yada.plugin.Gatekeeper}
    */
-  public static final String RX_IDX_INJECTION = "(([0-9]+):)?(get[A-Z][a-zA-Z0-9_]+\\([A-Za-z0-9_]*\\))";
+  public static final String RX_IDX_INJECTION = "(?:([0-9]+):)?(get[A-Z][a-zA-Z0-9_]+\\(([A-Za-z0-9_]*)\\))";
   
   
   /**
    * Constant equal to {@value}
    * @since 9.0.0
    */
-  public final static String RX_COL = "^(("+RX_COL_INJECTION+"|[A-Za-z0-9_]+))$";
+  public final static String RX_COL = "^"+RX_COL_INJECTION+"|[A-Za-z0-9_]+$";
   
   /**
    * Constant equal to {@value}
    * @since 9.0.0
    */
-  public final static String RX_IDX = "^(("+RX_IDX_INJECTION+"|[\\d]+))$";
+  public final static String RX_IDX = "^"+RX_IDX_INJECTION+"|[\\d]+$";
   
   /**
    * 
@@ -241,7 +243,7 @@ public class YADASecuritySpec extends HashMap<String, Object> {
     if(null == type || null == protector 
         || (!isColumns && list.stream().filter(c -> c.matches(RX_IDX)).collect(Collectors.toList()).size() == 0))
       throw new YADAQueryConfigurationException("policy, type, protector, and columns, indexes, or indices must be non-null");
-    this.put(KEY_POLICY, POLICY_EXECUTION);
+//    this.put(KEY_POLICY, POLICY_EXECUTION);
     this.put(KEY_TYPE, type);
     this.put(KEY_PROTECTOR, protector);
     if(isColumns)
@@ -253,6 +255,13 @@ public class YADASecuritySpec extends HashMap<String, Object> {
       this.put(KEY_INDEXES, columnsOrIndexes);
     }
   }
+  /**
+   * @return {@code true} if {@link #KEY_PROTECTOR} is present in the specification
+   */
+  public boolean hasExecutionPolicy()
+  {
+    return (this.get(KEY_PROTECTOR) != null);
+  }
   
   /**
    * @return
@@ -260,17 +269,18 @@ public class YADASecuritySpec extends HashMap<String, Object> {
   public Map<String, Object> getExecutionPolicy()  
   {
     Map<String,Object> policy = null;
-    if(this.get(KEY_POLICY) == POLICY_EXECUTION)
+//    if(this.get(KEY_POLICY) == POLICY_EXECUTION)
+    if(hasExecutionPolicy())
     {
       policy = new HashMap<String,Object>();
       policy.put(KEY_TYPE, (String) this.get(KEY_TYPE));
       policy.put(KEY_PROTECTOR, (String) this.get(KEY_PROTECTOR));
       if(this.get(KEY_COLUMNS) != null)
-        policy.put(KEY_COLUMNS, (List<?>) this.get(KEY_COLUMNS));
+        policy.put(KEY_COLUMNS, getValueAsList(this.get(KEY_COLUMNS)));
       else if(this.get(KEY_INDEXES) != null)
-        policy.put(KEY_INDEXES, (List<?>) this.get(KEY_INDEXES));
+        policy.put(KEY_INDEXES, getValueAsList(this.get(KEY_INDEXES)));
       else
-        policy.put(KEY_INDEXES, (List<?>) this.get(KEY_INDICES));
+        policy.put(KEY_INDEXES, getValueAsList(this.get(KEY_INDICES)));
       
     }
     return policy;
@@ -287,9 +297,17 @@ public class YADASecuritySpec extends HashMap<String, Object> {
     List<String> list = (List<String>) qualifier;
     if(null == type || null == qualifier || null == list || list.size() == 0)
       throw new YADAQueryConfigurationException("policy, type, protector, and columns, indexes, or indices must be non-null");
-    this.put(KEY_POLICY, POLICY_AUTHORIZATION);
+//    this.put(KEY_POLICY, POLICY_AUTHORIZATION);
     this.put(KEY_TYPE, type);
     this.put(KEY_QUALIFIER, qualifier);    
+  }
+  
+  /**
+   * @return {@code true} if {@link #KEY_QUALIFIER} is present in the specification
+   */
+  public boolean hasAuthorizationPolicy()
+  {
+    return (this.get(KEY_QUALIFIER) != null);
   }
   
   /**
@@ -298,11 +316,11 @@ public class YADASecuritySpec extends HashMap<String, Object> {
   public Map<String,Object> getAuthorizationPolicy()  
   {
     Map<String,Object> policy = null;
-    if(this.get(KEY_POLICY) == POLICY_AUTHORIZATION)
+    if(hasAuthorizationPolicy())
     {
       policy = new HashMap<String,Object>();
       policy.put(KEY_TYPE, (String) this.get(KEY_TYPE));
-      policy.put(KEY_QUALIFIER, (List<?>) this.get(KEY_QUALIFIER));
+      policy.put(KEY_QUALIFIER, getValueAsList(this.get(KEY_QUALIFIER)));
     }
     return policy;
   }
@@ -314,11 +332,19 @@ public class YADASecuritySpec extends HashMap<String, Object> {
    */
   public void setContentPolicy(String type, String predicate) throws YADAQueryConfigurationException 
   {
-    if(null == type || null == predicate)
+    if(null == predicate)
       throw new YADAQueryConfigurationException("policy, type, protector, and columns, indexes, or indices must be non-null");
-    this.put(KEY_POLICY, POLICY_CONTENT);
+//    this.put(KEY_POLICY, POLICY_CONTENT);
     this.put(KEY_TYPE, type);
     this.put(KEY_PREDICATE, predicate);    
+  }
+  
+  /**
+   * @return {@code true} if {@link #KEY_PREDICATE} is present in the specification
+   */
+  public boolean hasContentPolicy()
+  {
+    return (this.get(KEY_PREDICATE) != null);
   }
   
   /**
@@ -327,12 +353,23 @@ public class YADASecuritySpec extends HashMap<String, Object> {
   public Map<String,String> getContentPolicy()  
   {
     Map<String,String> policy = null;
-    if(this.get(KEY_POLICY) == POLICY_CONTENT)
+//    if(this.get(KEY_POLICY) == POLICY_CONTENT)
+    if(hasContentPolicy())
     {
       policy = new HashMap<String,String>();
       policy.put(KEY_TYPE, (String) this.get(KEY_TYPE));
       policy.put(KEY_PREDICATE, (String) this.get(KEY_PREDICATE));
     }
     return policy;
+  }
+  
+  /**
+   * @param value
+   * @return
+   */
+  private List<?> getValueAsList(Object value) {
+    if(null == value)
+      return null;
+    return Arrays.asList(((JSONArray)value).toString().split(","));
   }
 }
