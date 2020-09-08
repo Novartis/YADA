aff8ee3bc23db8116f3ed7e9b1ef4578b0c1b42d
David Varon <varontron@gmail.com>
2020-09-07 23:22:57 -0400

    enabled suppression of surefire tests or failsafe tests/cargo build with CLI options

--- src/main/resources/yadaMvnTestNG.sh
+++ src/main/resources/yadaMvnTestNG.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-x surefire|failsafe|all] [-p test|test_pre9] [-Xtd]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-T surefire|failsafe] [-x surefire|failsafe] [-p test|test_pre9] [-Xtd]" 1>&2; exit 1; }
 
 SUSPEND=n
 MAVEN_DEBUG=
 TOGGLE_TESTS=
 LOG_LEVEL="-Dlog.level=info"
-SUREFIRE=
-FAILSAFE=
+SUREFIRE_X=
+FAILSAFE_X=
 DEBUG=
 PROFILE=test
 YADA_PROPS=
+SKIP_SUREFIRE=
+SKIP_FAILSAFE=
 
-while getopts "x:Xtdp:" opt; do
+while getopts "x:Xtdp:T:" opt; do
   case ${opt} in
+    T )
+      if [ "surefire" == "$OPTARG" ]
+      then
+        SKIP_SUREFIRE=
+        SKIP_FAILSAFE="-Dskip.tests=true -Dskip.deploy.war=true"
+      elif [ "failsafe" == "$OPTARG" ]
+      then
+        SKIP_SUREFIRE=-Dsurefire.skip=true
+        SKIP_FAILSAFE=
+      elif [ -z "$OPTARG" ]
+      then
+        SKIP_SUREFIRE=
+        SKIP_FAILSAFE=
+      fi
+      ;;
     x )
       SUSPEND=y
       if [ "surefire" == "$OPTARG" ]
       then
-        SUREFIRE=true
+        SUREFIRE_X=true
       elif [ "failsafe" == "$OPTARG" ]
       then
-        FAILSAFE=true
+        FAILSAFE_X=true
       elif [ "all" = "$OPTARG" ] || [ -z "$OPTARG"]
       then
-        SUREFIRE=true
-        FAILSAFE=true
+        SUREFIRE_X=true
+        FAILSAFE_X=true
       fi
       ;;
     X )
@@ -71,6 +88,7 @@ COMMON_VARS="\
 $LOG_STDOUT \
 $MVN_DEPLOYMENT_GOAL \
 $SKIP_SUREFIRE \
+$SKIP_FAILSAFE \
 $SKIP_LICENSE \
 $SKIP_DB_LOAD \
 $SKIP_JAVADOC \
@@ -90,10 +108,10 @@ then
   # in one of the tools, e.g., YADA.properties
   SUREFIRE_DEBUG="-Dmaven.surefire.debug"
   FAILSAFE_DEBUG="-Dmaven.failsafe.debug"
-  if [ ! -z "${SUREFIRE}" ]
+  if [ ! -z "${SUREFIRE_X}" ]
   then
       DEBUG="${DEBUG} ${SUREFIRE_DEBUG}"
-  elif [ ! -z "${FAILSAFE}" ]
+  elif [ ! -z "${FAILSAFE_X}" ]
   then
       DEBUG="${DEBUG} ${FAILSAFE_DEBUG}"
   fi
